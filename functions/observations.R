faf0697d5750a73577832f550c2fef000cd3dc56
tenzytaec26 <tcthin27@colby.edu>
2025-01-17 15:39:26 -0500

    Making Commits.

--- functions/observations.R
+++ functions/observations.R
@@ -1,4 +1,4 @@
-read_observations = function(scientificname = "Mola mola",
+read_observations = function(scientificname = "Hemitripterus americanus",
                              minimum_year = 1970, 
                              ...){
   
@@ -15,22 +15,24 @@ read_observations = function(scientificname = "Mola mola",
   obs = read_obis(scientificname, ...)
   
   # Print initial summary
-  print("Raw data preview:")
-  print(summary(obs))
+  #print("Raw data preview:")
+  #print(summary(obs))
   
   dim_start = dim(obs)
   
-  print("Initial count of basisOfRecord:")
-  print(obs |> count(basisOfRecord))
+  #print("Initial count of basisOfRecord:")
+  #print(obs |> count(basisOfRecord))
   
   # Remove rows where eventDate is missing
-  obs = obs |> filter(!is.na(eventDate))
+  obs = obs |> 
+    filter(!is.na(eventDate))
   
   # Debugging: Show an example row with missing individualCount before filtering
-  missing_individual_count = obs |> filter(is.na(individualCount)) |> slice(1)
+  #missing_individual_count = obs |> filter(is.na(individualCount)) |> slice(1)
   
   # Remove rows where individualCount is missing
-  obs = obs |> filter(!is.na(individualCount))
+  obs = obs |> 
+    filter(!is.na(individualCount))
  
   # Apply filtering by year if minimum_year is provided
   if (!is.null(minimum_year)){
@@ -39,8 +41,8 @@ read_observations = function(scientificname = "Mola mola",
   }
   
   # Final summary
-  print("Final summary of observations:")
-  print(summary(obs))
+  #print("Final summary of observations:")
+  #print(summary(obs))
   
   return(obs)
 }
